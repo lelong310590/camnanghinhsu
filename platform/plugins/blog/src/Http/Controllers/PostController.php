ca1903bb9079262a862077e07d86a998ea94ec67
root <root@163-44-193-158.conoha.io>
2023-04-25 03:29:29 +0700

    initial commit

---
+++ platform/plugins/blog/src/Http/Controllers/PostController.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace Botble\Blog\Http\Controllers;
+
+use Botble\ACL\Models\User;
+use Botble\Base\Events\BeforeEditContentEvent;
+use Botble\Base\Events\CreatedContentEvent;
+use Botble\Base\Events\DeletedContentEvent;
+use Botble\Base\Events\UpdatedContentEvent;
+use Botble\Base\Forms\FormBuilder;
+use Botble\Base\Http\Controllers\BaseController;
+use Botble\Base\Http\Responses\BaseHttpResponse;
+use Botble\Base\Traits\HasDeleteManyItemsTrait;
+use Botble\Blog\Forms\PostForm;
+use Botble\Blog\Http\Requests\PostRequest;
+use Botble\Blog\Models\Post;
+use Botble\Blog\Repositories\Interfaces\CategoryInterface;
+use Botble\Blog\Repositories\Interfaces\PostInterface;
+use Botble\Blog\Repositories\Interfaces\TagInterface;
+use Botble\Blog\Services\StoreCategoryService;
+use Botble\Blog\Services\StoreTagService;
+use Botble\Blog\Tables\PostTable;
+use Exception;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\View\View;
+use Throwable;
+
+class PostController extends BaseController
+{
+    use HasDeleteManyItemsTrait;
+
+    /**
+     * @var PostInterface
+     */
+    protected $postRepository;
+
+    /**
+     * @var TagInterface
+     */
+    protected $tagRepository;
+
+    /**
+     * @var CategoryInterface
+     */
+    protected $categoryRepository;
+
+    /**
+     * @param PostInterface $postRepository
+     * @param TagInterface $tagRepository
+     * @param CategoryInterface $categoryRepository
+     */
+    public function __construct(
+        PostInterface $postRepository,
+        TagInterface $tagRepository,
+        CategoryInterface $categoryRepository
+    ) {
+        $this->postRepository = $postRepository;
+        $this->tagRepository = $tagRepository;
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @param PostTable $dataTable
+     * @return Factory|View
+     * @throws Throwable
+     */
+    public function index(PostTable $dataTable)
+    {
+        page_title()->setTitle(trans('plugins/blog::posts.menu_name'));
+
+        return $dataTable->renderTable();
+    }
+
+    /**
+     * @param FormBuilder $formBuilder
+     * @return string
+     */
+    public function create(FormBuilder $formBuilder)
+    {
+        page_title()->setTitle(trans('plugins/blog::posts.create'));
+
+        return $formBuilder->create(PostForm::class)->renderForm();
+    }
+
+    /**
+     * @param PostRequest $request
+     * @param StoreTagService $tagService
+     * @param StoreCategoryService $categoryService
+     * @param BaseHttpResponse $response
+     * @return BaseHttpResponse
+     */
+    public function store(
+        PostRequest $request,
+        StoreTagService $tagService,
+        StoreCategoryService $categoryService,
+        BaseHttpResponse $response
+    ) {
+        /**
+         * @var Post $post
+         */
+        $post = $this->postRepository->createOrUpdate(array_merge($request->input(), [
+            'author_id'   => Auth::id(),
+            'author_type' => User::class,
+            'search_name' => $this->convert_name($request->input('name'))
+        ]));
+  
+        
+        event(new CreatedContentEvent(POST_MODULE_SCREEN_NAME, $request, $post));
+
+        $tagService->execute($request, $post);
+
+        $categoryService->execute($request, $post);
+
+        return $response
+            ->setPreviousUrl(route('posts.index'))
+            ->setNextUrl(route('posts.edit', $post->id))
+            ->setMessage(trans('core/base::notices.create_success_message'));
+    }
+    
+    public function convert_name($str) {
+    		$str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+    		$str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+    		$str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+    		$str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+    		$str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+    		$str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+    		$str = preg_replace("/(đ)/", 'd', $str);
+    		$str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+    		$str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+    		$str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+    		$str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+    		$str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+    		$str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+    		$str = preg_replace("/(Đ)/", 'D', $str);
+    		$str = preg_replace("/(\“|\”|\‘|\’|\,|\!|\&|\;|\@|\#|\%|\~|\`|\=|\_|\'|\]|\[|\}|\{|\)|\(|\+|\^)/", ' ', $str);
+    		$str = preg_replace("/( )/", ' ', $str);
+    		return $str;
+    	}
+    /**
+     * @param int $id
+     * @param FormBuilder $formBuilder
+     * @param Request $request
+     * @return string
+     */
+    public function edit($id, FormBuilder $formBuilder, Request $request)
+    {
+        $post = $this->postRepository->findOrFail($id);
+
+        event(new BeforeEditContentEvent($request, $post));
+
+        page_title()->setTitle(trans('plugins/blog::posts.edit') . ' "' . $post->name . '"');
+
+        return $formBuilder->create(PostForm::class, ['model' => $post])->renderForm();
+    }
+
+    /**
+     * @param int $id
+     * @param PostRequest $request
+     * @param StoreTagService $tagService
+     * @param StoreCategoryService $categoryService
+     * @param BaseHttpResponse $response
+     * @return BaseHttpResponse
+     */
+    public function update(
+        $id,
+        PostRequest $request,
+        StoreTagService $tagService,
+        StoreCategoryService $categoryService,
+        BaseHttpResponse $response
+    ) {
+        $post = $this->postRepository->findOrFail($id);
+
+        $post->fill($request->input());
+        
+        $post->search_name = $this->convert_name($request->input('name'));
+        
+        $this->postRepository->createOrUpdate($post);
+
+        event(new UpdatedContentEvent(POST_MODULE_SCREEN_NAME, $request, $post));
+
+        $tagService->execute($request, $post);
+
+        $categoryService->execute($request, $post);
+
+        return $response
+            ->setPreviousUrl(route('posts.index'))
+            ->setMessage(trans('core/base::notices.update_success_message'));
+    }
+
+    /**
+     * @param int $id
+     * @param Request $request
+     * @return BaseHttpResponse
+     */
+    public function destroy($id, Request $request, BaseHttpResponse $response)
+    {
+        try {
+            $post = $this->postRepository->findOrFail($id);
+            $this->postRepository->delete($post);
+
+            event(new DeletedContentEvent(POST_MODULE_SCREEN_NAME, $request, $post));
+
+            return $response
+                ->setMessage(trans('core/base::notices.delete_success_message'));
+        } catch (Exception $exception) {
+            return $response
+                ->setError()
+                ->setMessage($exception->getMessage());
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param BaseHttpResponse $response
+     * @return BaseHttpResponse
+     * @throws Exception
+     */
+    public function deletes(Request $request, BaseHttpResponse $response)
+    {
+        return $this->executeDeleteItems($request, $response, $this->postRepository, POST_MODULE_SCREEN_NAME);
+    }
+
+    /**
+     * @param Request $request
+     * @param BaseHttpResponse $response
+     * @return BaseHttpResponse
+     * @throws Throwable
+     */
+    public function getWidgetRecentPosts(Request $request, BaseHttpResponse $response)
+    {
+        $limit = (int)$request->input('paginate', 10);
+        $limit = $limit > 0 ? $limit : 10;
+
+        $posts = $this->postRepository->advancedGet([
+            'with'     => ['slugable'],
+            'order_by' => ['created_at' => 'desc'],
+            'paginate' => [
+                'per_page'      => $limit,
+                'current_paged' => (int)$request->input('page', 1),
+            ],
+        ]);
+
+        return $response
+            ->setData(view('plugins/blog::posts.widgets.posts', compact('posts', 'limit'))->render());
+    }
+}
